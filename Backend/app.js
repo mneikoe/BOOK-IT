582d32de1d57407b243885fd3d126720115c52d0
AMAN RAJ <raj117557@gmail.com>
2024-12-07 15:17:23 +0530

    7 dec 2024

--- Backend/app.js
+++ Backend/app.js
@@ -7,10 +7,16 @@ const cors = require("cors");
 app.use(cors());
 
 const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 connectToDb();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send({ message: "Welcome to the UBER API!" });
+  res.send({ message: "Welcme to the UBER API!" });
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,10 +1,17 @@
+// db.js
 const mongoose = require("mongoose");
 
+//const uri = "mongodb://127.0.0.1:4000";
+const uri = "mongodb://0.0.0.0/UBER-PRACTICE";
+
 function connectToDb() {
-  mongoose
-    .connect("mongodb://localhost:4000/uber-practice")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Could not connect to MongoDB", err));
+  try {
+    mongoose.connect(uri);
+    console.log("Connected successfully to database");
+  } catch (err) {
+    console.error("Connection to database failed", err);
+    throw err;
+  }
 }
 
 module.exports = connectToDb;
