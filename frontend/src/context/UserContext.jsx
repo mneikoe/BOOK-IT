e05c15f38bca6ca7a3aea2d0420897148d6d31eb
AMAN RAJ <raj117557@gmail.com>
2024-12-14 17:28:40 +0530

    login signup pages

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const UserDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  return (
+    <div>
+      <UserDataContext.Provider value={[user, setUser]}>
+        {children}
+      </UserDataContext.Provider>
+    </div>
+  );
+};
+UserContext.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default UserContext;
---
+++ frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,115 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const CaptainSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [captainData, setCaptainData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCaptainData({
+      email: email,
+      password: password,
+      fullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+    });
+    console.log(captainData);
+    setEmail("");
+    setPassword("");
+    setFirstName("");
+    setLastName("");
+  };
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What is our Captain Name</h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              className="bg-[#eeee] w-1/2 h-15 rounded px-4 py-2 border  text-base placceholder:text-sm"
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <input
+              className="bg-[#eeee] w-1/2 rounded px-4 py-2 border  text-base placceholder:text-sm"
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+          </div>
+
+          <h3 className="text-lg font-medium mb-2">
+            What is Our Captain Email
+          </h3>
+          <input
+            required
+            className="bg-[#eeee] mb-6 rounded px-4 py-2 border w-full text-base placceholder:text-sm"
+            type="email"
+            placeholder="Email@example.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+
+          <input
+            required
+            className="bg-[#eeee] mb-6 rounded px-4 py-2 border w-full text-lg placceholder:text-base"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+
+          <button
+            className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placceholder:text-base"
+            type="submit"
+          >
+            Login
+          </button>
+          <p className="text-center">
+            Already have a Account ?{" "}
+            <Link to="/captain-login" className="text-blue-600">
+              Login as Captain
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <p className="text-[10px]">
+          sahfah ahfk shas khcvakvs kahfk sdfk hf sdfjhsdf skfh sddhf ahsfjs
+          dmnvkn hwiuefh kh fsiuafye dfhsui afy iufheiuf ashfsahf uiweryvb kkf
+          kjjhsdfu aksjjdf adsfh ajsdfh asefh{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
